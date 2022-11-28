5aaac3e8ac4e70ab61da17b9c3a8a5ea222292f5
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-11-28 20:25:31 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -16,7 +16,7 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    assert(majorColor[0] == "White")
+    assert(result == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
