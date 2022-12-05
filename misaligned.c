926bb444cb0a4d6163e85db361042a572439d033
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-12-05 23:00:52 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -22,7 +22,7 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d , %s , %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            printf("%d , %s , %s\n", i * 5 + j, majorColorNames[i], minorColorNames[j]);
         }
     }
     return i * j;
