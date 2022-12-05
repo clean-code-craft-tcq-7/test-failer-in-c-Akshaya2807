f88a833fdd5c35a46cbbe33654f5c9570855405a
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-12-05 22:58:53 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -19,8 +19,6 @@ typedef struct {
 } ColorPair;
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
